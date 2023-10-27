a9b7215b91e46be671a207c99e6d564f20029c1e
Éderson Fernandes <efernandes.tech@gmail.com>
2023-10-26 23:36:50 -0300

    CRUD tasks in memory

--- MyTasksMinimalApi/MyTasksMinimalApi/Program.cs
+++ MyTasksMinimalApi/MyTasksMinimalApi/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+    opt.UseInMemoryDatabase("MyTasksDB"));
 
 var app = builder.Build();
 
@@ -17,4 +22,65 @@ app.MapGet("phrases", async () =>
     await new HttpClient().GetStringAsync("https://ron-swanson-quotes.herokuapp.com/v2/quotes")
 );
 
+app.MapGet("/tasks", async (AppDbContext db) =>
+{
+    return await db.Tasks.ToListAsync();
+});
+
+app.MapGet("/tasks/{id:int}", async (int id, AppDbContext db) =>
+    await db.Tasks.FindAsync(id) is Task task
+        ? Results.Ok(task)
+        : Results.NotFound());
+
+app.MapGet("/tasks/complete", async (AppDbContext db) =>
+{
+    return await db.Tasks.Where(t => t.IsComplete).ToListAsync();
+});
+
+app.MapPost("/tasks", async (Task task, AppDbContext db) =>
+{
+    db.Tasks.Add(task);
+    await db.SaveChangesAsync();
+    return Results.Created($"/tasks/{task.Id}", task);
+});
+
+app.MapPut("/tasks/{id:int}", async (int id, Task inputTask, AppDbContext db) =>
+{
+    var task = await db.Tasks.FindAsync(id);
+    if (task is null)
+        return Results.NotFound();
+
+    task.Name = inputTask.Name;
+    task.IsComplete = inputTask.IsComplete;
+
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+app.MapDelete("/tasks/{id:int}", async (int id, AppDbContext db) =>
+{
+    if (await db.Tasks.FindAsync(id) is Task task)
+    {
+        db.Tasks.Remove(task);
+        await db.SaveChangesAsync();
+        return Results.Ok(task);
+    }
+    return Results.NotFound();
+});
+
 app.Run();
+
+public class Task
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+}
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    { }
+
+    public DbSet<Task> Tasks => Set<Task>();
+}
